7f8bbb96c2a7dea19c3a704738873fa0a0d7d4bf
Vaibhav Singh <89960983+vaibhavvsingh@users.noreply.github.com>
2025-01-26 11:31:47 +0530

    Adding env variable for API URL

--- src/static/constants.ts
+++ src/static/constants.ts
@@ -1,5 +1,5 @@
 // const backendUrl = "https://www.hjsfdjjksdf.in/";
 // const backendUrl = "http://localhost:3500/";
-const backendUrl = "https://4ubx2d4dvm.ap-southeast-1.awsapprunner.com/";
+const backendUrl = import.meta.env.VITE_API_URL;
 
 export default backendUrl;
